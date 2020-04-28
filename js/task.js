b8e79f6600284200db18448ab0e186de67099e7a
hrushkooleksii <hrushkooleksii@gmail.com>
2020-04-28 15:58:42 +0300

    fixing task-03

--- js/task.js
+++ js/task.js
@@ -23,7 +23,7 @@ console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley
 
 // Получить массив имен пользователей по полу (поле gender).
 const getUsersWithGender = (users, gender) => {
-  return users.filter(user => user.gender === gender);
+  return users.filter(user => user.gender === gender).map(user => user.name);
 };
 
 console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
